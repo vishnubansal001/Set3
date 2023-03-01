c450ea46db7f0c4f2f2e2f9f6c78e0c73f4b5ff8
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-03-01 12:25:38 +0530

    first commit

---
+++ src/components/ThirdPart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import img from '../assests/screen.svg';
+
+const ThirdPart = () => {
+  return (
+    <div>
+      {/* <img src={img} alt="img/svg" /> */}
+    </div>
+  )
+}
+
+export default ThirdPart
---
+++ src/data/data.js
@@ -0,0 +1,43 @@
+const appStudio1 = {
+    title:"Why Choose",
+    subtitle: "AppStudio?",
+    text: "We are Canada's top-rated mobile application developer. With hundreds of chart-topping applications delivered to startups, businesses and enterprises worldwide, we have emerged as pioneers of digital transformation and tactical disruption. Our experts have many years of skills and knowledge under their belt, which enables them to produce top-notch business solutions that deliver remarkable digital experiences.",
+    boxes:[
+        {text:"Absolute Support"},
+        {text:"Complete Technical Competency"}
+    ]
+};
+
+const appStudio2 = {
+    boxe1:[
+        {text:"Excellent Quality Delivered on Time"},
+        {text:"Full Confidentiality"}
+    ],
+    boxe2:[
+        {text:"Effective & Continuous Communication"},
+        {text:"Friendly & Cordial in Nature"},
+        {text:"Experienced Team"}
+    ]
+};
+
+const kindApp = {
+    title:"Kind of",
+    subtitle:"Apps",
+    text:"Here, We make almost every genre of applications",
+    boxes:[
+        {text:"Healthcare Industry"},
+        {text:"Fitness & Health"},
+        {text:"On-Demand Service"},
+        {text:"Education"},
+        {text:"Beauty & Fashion"},
+        {text:"Enterprises Applications"},
+        {text:"Entertainment program"},
+        {text:"Retail & E-commerce"},
+        {text:"Social Network"},
+        {text:"Government Services"}
+    ]
+};
+
+const community = {
+    
+}
